0caeaedfdd27d2954e340795d1b0907df16ebf3f
LIL_NAZ <carljhenriksson@gmail.com>
2015-04-26 10:52:05 +0200

    added parallax header
    added parallax header & gram img
    fix js path
    updated jquery

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,10 @@
-$( document ).ready(function() {
+$(window).scroll(function(){
+	var wScroll = $(this).scrollTop();
 
-  //
-
+	$('.logo').css({
+		'margin-top' : '' + wScroll /2 +'px'
+	})
+	$('.fore-gram').css({
+		'margin-top' : '' + wScroll*1.5 +'px'
+	});
 });
